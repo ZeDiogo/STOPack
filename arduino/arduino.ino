be9655a751ae00b7310bac86fdb9ae7fafc23e9a
ZeDiogo <zediogooliveira@hotmail.com>
2018-11-14 15:45:14 +0000

    implemented led bar, increment/dec buttons

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -18,70 +18,149 @@
 */
 
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
-const int statusPin = 7;
+const int triggerPin = 2;     // the number of the pushbutton pin
+//const int ledPin =  13;      // the number of the LED pin
+const int statusPin = 3;
+const int incPin = 1; //analog
+const int decPin = 0; //analog
+const int RED = 1;
+//const int GREEN = 2;
+const int BLUE = 3;
 
 // variables will change:
 int lidState = 0;
+int incState = LOW;
+int decState = LOW;
 int prevLidState = 0;
 int prevStatusState = 0;
+int prevIncState = LOW;
+int prevDecState = LOW;
 int statusState = 0;
-int intervalCigarettes = 5;
+//int intervalCigarettes = 5;
 int smoked = 0;
-unsigned long lastCigarTime = 0;
+//unsigned long lastCigarTime = 0;
+int limit = 5;
+
+int ledPins[] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4};
+int leds[] = {BLUE, BLUE, BLUE, BLUE, BLUE};
+int ledSignals[] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
+int LEDS_SIZE = 5;
+int LEDS_SIGNALS_SIZE = 10;
+int PERCENTAGE_DIVISION = 100 / LEDS_SIZE;
 
 void setup() {
   // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
+  int i;
+  for (i = 0; i < LEDS_SIGNALS_SIZE; i++) {
+    pinMode(ledPins[i], OUTPUT); 
+  }
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(triggerPin, INPUT);
   pinMode(statusPin, INPUT);
+  //pinMode(incPin, INPUT);
+  //pinMode(decPin, INPUT);
   Serial.begin(9600);
 }
 
-void signalUnlocked() {
-  delay(100);
-  digitalWrite(ledPin, HIGH);
-  delay(2000);
-  digitalWrite(ledPin, LOW);
+void resetLeds() {
+  int i;
+  for (i = 0; i < LEDS_SIZE; i++) {
+    leds[i] = BLUE;
+  }
+  for (i = 0; i < LEDS_SIGNALS_SIZE; i++) {
+    ledSignals[i] = LOW;
+  }
 }
 
-void signalLocked() {
-  delay(100);
-  for (int i = 0; i < 2; i++) {
-    digitalWrite(ledPin, HIGH);
-    delay(200);
-    digitalWrite(ledPin, LOW);
-    delay(150);
+void convertPercent2Colors() {
+  int percentage = (int) (smoked * 100 / limit);
+  //Serial.print("Percentage: ");
+  //Serial.print(percentage);
+  resetLeds();
+  /*0-20 0/5 reds (all blues)
+  20-40 1/5 reds
+  40.60 2/5 reds
+  60-80 3/5 reds
+  80-100 4/5 reds
+  100 - all reds*/
+  int maxSize = percentage / PERCENTAGE_DIVISION;
+  int i;
+  if (maxSize > LEDS_SIZE) { maxSize = LEDS_SIZE; }
+  for (i = 0; i < maxSize; i++) {
+    leds[i] = RED;
   }
-  digitalWrite(ledPin, HIGH);
-  delay(300);
-  digitalWrite(ledPin, LOW);
+}
+
+void mapLeds() {
+  int i;
+  //Serial.print(" LEDS: ");
+  for (i = 0; i < LEDS_SIZE; i++) {
+    if (leds[i] == BLUE) {
+      //Serial.print("B"); 
+      ledSignals[i*2] = HIGH;
+      ledSignals[i*2+1] = LOW;
+    } else {
+      //Serial.print("R");
+      ledSignals[i*2] = LOW;
+      ledSignals[i*2+1] = HIGH;
+    }
+  }
+}
+
+void showStatus() {
+  //Serial.print(" LEDS: ");
+  delay(100);
+  for (int i = 0; i < LEDS_SIGNALS_SIZE; i++) {
+    //Serial.print(ledSignals[i]);
+    digitalWrite(ledPins[i], ledSignals[i]);
+  }
+  delay(2000);
+  for (int i = 0; i < LEDS_SIGNALS_SIZE; i++) {
+    digitalWrite(ledPins[i], LOW);
+  }
 }
 
 void loop() {
-  lidState = digitalRead(buttonPin);
+  lidState = digitalRead(triggerPin);
   statusState = digitalRead(statusPin);
-
+  incState = analogRead(incPin);
+  decState = analogRead(decPin);
+  //Serial.print("Analog inc: ");
+  //Serial.print(incState);
+  
   if (lidState == LOW && prevLidState == HIGH) {
     //increment number cigarettes;
     smoked = smoked + 1;
-    Serial.print("Smoked:");
+    convertPercent2Colors();
+    Serial.print("\nSmoked:");
     Serial.print(smoked);
-    lastCigarTime = millis();
+    //lastCigarTime = millis();
   }
-  if (statusState == LOW && prevStatusState == HIGH) {
-    long now = millis();
-    Serial.print(now + '>' + (lastCigarTime + intervalCigarettes * 1000));
-    if (now > (lastCigarTime + intervalCigarettes * 1000)) {
-      Serial.print("Unlocked\n");
-      signalUnlocked();
+  else if (statusState == LOW && prevStatusState == HIGH) {
+    mapLeds();
+    showStatus();
+  }
+  //Serial.print("\nincState = ");
+  //Serial.print(incState);
+  else if (incState == LOW && prevIncState > HIGH) {
+    limit = limit + 1;
+    convertPercent2Colors();
+    Serial.print("\nIncremented limit\nNew limit: ");
+    Serial.print(limit);
+  }
+  else if (decState == LOW && prevDecState > HIGH) {
+    if (limit > 1) {
+      limit = limit - 1;
+      convertPercent2Colors();
+      Serial.print("\nIncremented limit\nNew limit: ");
+      Serial.print(limit);
     } else {
-      Serial.print("Locked\n");
-      signalLocked();
+      Serial.print("\nReached bottom limit 1");
     }
   }
+  
   prevLidState = lidState;
   prevStatusState = statusState;
+  prevIncState = incState;
+  prevDecState = decState;
 }
